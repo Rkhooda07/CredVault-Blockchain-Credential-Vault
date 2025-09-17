34bf503c6f5ba9cd4631411c7eb4274ab4272f1f
Rakshit Hooda <rakshit.official703@gmail.com>
2025-09-17 10:05:11 +0530

    Add files via upload

--- Navigation.tsx
+++ Navigation.tsx
@@ -25,7 +25,7 @@ export function Navigation() {
             <div className="w-8 h-8 bg-gradient-hero rounded-lg flex items-center justify-center">
               <Shield className="w-5 h-5 text-primary-foreground" />
             </div>
-            <span className="font-bold text-xl text-foreground">CredVault</span>
+            <span className="font-bold text-xl text-foreground">CertifiedChain</span>
           </Link>
 
           {/* Desktop Navigation */}
